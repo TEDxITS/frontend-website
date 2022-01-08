5a4e15bdfe80030a2b0041d2fd65175ee87d523e
winatungmiharja <winatungmiharja@gmail.com>
2022-01-08 16:30:09 +0900

    fix: prettier format

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,12 +6,11 @@ import Seo from '@/components/Seo';
 export default function HomePage() {
   return (
     <Layout>
-      {/* <Seo templateTitle='Home' /> */}
-      <Seo />
+      <Seo templateTitle='Sandbox' />
 
       <main>
         <section className='bg-cdark text-clight'>
-          <div className='flex flex-col items-center justify-center layout min-h-screen text-center'>
+          <div className='layout flex flex-col justify-center items-center min-h-screen text-center'>
             <h1 className='mt-4'>TEDxITS 2022</h1>
             <p className='mt-2 text-sm'>A starter configuration project</p>
 
@@ -19,7 +18,7 @@ export default function HomePage() {
               <ul className='flex flex-col gap-8'>
                 <h2 className='text-primary-300'>
                   Sandbox{' '}
-                  <span className='font-semibold text-lg'>for the website</span>
+                  <span className='text-lg font-semibold'>for the website</span>
                 </h2>
                 <hr />
                 <li>
@@ -27,28 +26,28 @@ export default function HomePage() {
                     00. Font Type
                     <p>
                       use{' '}
-                      <span className='bg-primary-300 px-2 py-1'>
+                      <span className='px-2 py-1 bg-primary-300'>
                         font-fivo
                       </span>{' '}
                       <span className='font-fivo'>for this font</span>
                     </p>
                     <p>
                       use{' '}
-                      <span className='bg-primary-300 px-2 py-1'>
+                      <span className='px-2 py-1 bg-primary-300'>
                         font-pilow
                       </span>{' '}
                       <span className='font-pilow'>for this font</span>
                     </p>
                     <p>
                       use{' '}
-                      <span className='bg-primary-300 px-2 py-1'>
+                      <span className='px-2 py-1 bg-primary-300'>
                         font-lucki
                       </span>{' '}
                       <span className='font-lucki'>for this font</span>
                     </p>
                     <p>
                       use{' '}
-                      <span className='bg-primary-300 px-2 py-1'>
+                      <span className='px-2 py-1 bg-primary-300'>
                         font-sympath
                       </span>{' '}
                       <span className='font-sympath'>for this font</span>
@@ -78,59 +77,59 @@ export default function HomePage() {
                 <hr />
                 <li>
                   02. Color Palette
-                  <div className='gap-2 grid grid-cols-1 md:grid-cols-2'>
+                  <div className='grid grid-cols-1 gap-2 md:grid-cols-2'>
                     <div className='flex flex-col gap-2'>
                       <p className='heading'>Main color</p>
-                      <div className='bg-primary-100 border-2 border-primary-100 p-4 shadow-xl w-full'>
+                      <div className='p-4 w-full bg-primary-100 border-2 border-primary-100 shadow-xl'>
                         primary-100
                       </div>
-                      <div className='bg-primary-200 border-2 border-primary-100 p-4 shadow-xl w-full'>
+                      <div className='p-4 w-full bg-primary-200 border-2 border-primary-100 shadow-xl'>
                         primary-200
                       </div>
-                      <div className='bg-primary-300 border-2 border-primary-100 p-4 shadow-xl w-full'>
+                      <div className='p-4 w-full bg-primary-300 border-2 border-primary-100 shadow-xl'>
                         primary-300{' '}
                       </div>
-                      <div className='bg-primary-400 border-2 border-primary-100 p-4 shadow-xl w-full'>
+                      <div className='p-4 w-full bg-primary-400 border-2 border-primary-100 shadow-xl'>
                         primary-400
                       </div>
-                      <div className='bg-primary-500 border-2 border-primary-100 p-4 shadow-xl w-full'>
+                      <div className='p-4 w-full bg-primary-500 border-2 border-primary-100 shadow-xl'>
                         primary-500{' '}
                       </div>
-                      <div className='bg-primary-600 border-2 border-primary-100 p-4 shadow-xl text-primary-100 w-full'>
+                      <div className='p-4 w-full text-primary-100 bg-primary-600 border-2 border-primary-100 shadow-xl'>
                         primary-600
                       </div>
-                      <div className='bg-primary-700 border-2 border-primary-100 p-4 shadow-xl text-primary-100 w-full'>
+                      <div className='p-4 w-full text-primary-100 bg-primary-700 border-2 border-primary-100 shadow-xl'>
                         primary-700{' '}
                       </div>
-                      <div className='bg-primary-800 border-2 border-primary-100 p-4 shadow-xl text-primary-100 w-full'>
+                      <div className='p-4 w-full text-primary-100 bg-primary-800 border-2 border-primary-100 shadow-xl'>
                         primary-800
                       </div>
-                      <div className='bg-primary-900 border-2 border-primary-100 p-4 shadow-xl text-primary-100 w-full'>
+                      <div className='p-4 w-full text-primary-100 bg-primary-900 border-2 border-primary-100 shadow-xl'>
                         primary-900
                       </div>
                     </div>
                     <div className='flex flex-col gap-2'>
                       <p className='heading'>Accent Color</p>
-                      <div className='bg-cdark border-2 border-primary-100 p-4 shadow-xl w-full'>
+                      <div className='bg-cdark p-4 w-full border-2 border-primary-100 shadow-xl'>
                         bg-cdark
                       </div>
-                      <div className='bg-clight border-2 border-primary-100 p-4 shadow-xl text-cdark w-full'>
+                      <div className='bg-clight text-cdark p-4 w-full border-2 border-primary-100 shadow-xl'>
                         bg-clight
                       </div>
-                      <div className='bg-cgreen border-2 border-primary-100 p-4 shadow-xl w-full'>
+                      <div className='bg-cgreen p-4 w-full border-2 border-primary-100 shadow-xl'>
                         bg-cgreen
                       </div>
-                      <div className='bg-cblue border-2 border-primary-100 p-4 shadow-xl w-full'>
+                      <div className='bg-cblue p-4 w-full border-2 border-primary-100 shadow-xl'>
                         bg-cblue
                       </div>
 
-                      <div className='bg-cyellow border-2 border-primary-100 p-4 shadow-xl text-cdark w-full'>
+                      <div className='bg-cyellow text-cdark p-4 w-full border-2 border-primary-100 shadow-xl'>
                         bg-cyellow
                       </div>
-                      <div className='bg-cpurple border-2 border-primary-100 p-4 shadow-xl w-full'>
+                      <div className='bg-cpurple p-4 w-full border-2 border-primary-100 shadow-xl'>
                         bg-cpurple
                       </div>
-                      <div className='bg-cpink border-2 border-primary-100 p-4 shadow-xl text-cdark w-full'>
+                      <div className='bg-cpink text-cdark p-4 w-full border-2 border-primary-100 shadow-xl'>
                         bg-cpink
                       </div>
                     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { fontFamily } = require('tailwindcss/defaultTheme');
-
 function withOpacity(variableName) {
   return ({ opacityValue }) => {
     if (opacityValue !== undefined) {
