fb827da174a93c5d8cdc47072c261c0bac904d1b
winatungmiharja <winatungmiharja@gmail.com>
2022-01-11 13:00:20 +0900

    feat: add navigation bar

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,29 +1,212 @@
+/* eslint-disable @next/next/no-img-element */
+
+import { motion } from 'framer-motion';
+import { Sling as Hamburger } from 'hamburger-react';
 import * as React from 'react';
 
-import UnstyledLink from '@/components/links/UnstyledLink';
+import clsxm from '@/lib/clsxm';
 
-const links = [
-  { href: '/', label: 'Route 1' },
-  { href: '/', label: 'Route 2' },
+import InstagramLogoIllustration from '@/assets/svg/InstagramLogoIllustration';
+import LinkedinLogoIllustration from '@/assets/svg/LinkedinLogoIllustration';
+import TwitterLogoIllustration from '@/assets/svg/TwitterLogoIllustration';
+import YoutubeLogoIllustration from '@/assets/svg/YoutubeLogoIllustration';
+
+import HeaderLink from '../links/HeaderLink';
+
+//#region  //*=========== Type ===========
+type LinkType = {
+  href: string;
+  label: string;
+};
+
+type SoialMediaLinkType = LinkType & {
+  logo: React.ReactNode;
+};
+
+type HeaderProps = {
+  topBreakpoint?: number;
+};
+//#endregion  //*======== Type ===========
+
+//#region  //*=========== Link Data ===========
+const pageLinks: LinkType[] = [
+  { href: '/', label: 'Home' },
+  { href: '/about', label: 'About' },
+  { href: '/committee', label: 'Our Committee' },
 ];
 
-export default function Header() {
+const socialMediaLinks: SoialMediaLinkType[] = [
+  {
+    href: '/',
+    label: 'Instagram',
+    logo: (
+      <InstagramLogoIllustration
+        className='h-10 md:h-7 hover:cursor-pointer'
+        pathClassName='duration-150 transition-all'
+      />
+    ),
+  },
+  {
+    href: '/',
+    label: 'Twitter',
+    logo: (
+      <TwitterLogoIllustration
+        className='h-10 md:h-7 hover:cursor-pointer'
+        pathClassName='duration-150 transition-all'
+      />
+    ),
+  },
+  {
+    href: '/',
+    label: 'LinkedIn',
+    logo: (
+      <LinkedinLogoIllustration
+        className='h-10 md:h-7 hover:cursor-pointer'
+        pathClassName='duration-150 transition-all'
+      />
+    ),
+  },
+  {
+    href: '/',
+    label: 'Youtube',
+    logo: (
+      <YoutubeLogoIllustration
+        className='h-10 md:h-7 hover:cursor-pointer'
+        pathClassName='duration-150 transition-all'
+      />
+    ),
+  },
+];
+//#endregion  //*======== Link Data ===========
+
+export default function Header({ topBreakpoint }: HeaderProps) {
+  //#region  //*=========== Navigation Mobile State ===========
+  const [isNavOpen, setIsNavOpen] = React.useState<boolean>(false);
+  //#endregion  //*======== Navigation Mobile State ===========
+
+  //#region  //*=========== Navigation Scrolled State ===========
+  const [isNavbarScrolled, setisNavbarScrolled] =
+    React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    document.addEventListener('scroll', () => {
+      window.scrollY < 64 + (topBreakpoint ? topBreakpoint : 0)
+        ? setisNavbarScrolled(false)
+        : setisNavbarScrolled(true);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  //#endregion  //*======== Navigation Scrolled State ===========
+
   return (
-    <header className='sticky top-0 z-50 bg-white'>
-      <div className='layout flex justify-between items-center h-14'>
-        <UnstyledLink href='/' className='font-bold hover:text-gray-600'>
-          Home
-        </UnstyledLink>
-        <nav>
-          <ul className='flex justify-between items-center space-x-4'>
-            {links.map(({ href, label }) => (
-              <li key={`${href}${label}`}>
-                <UnstyledLink href={href} className='hover:text-gray-600'>
-                  {label}
-                </UnstyledLink>
-              </li>
-            ))}
-          </ul>
+    <header
+      className={clsxm('sticky top-0 z-50 bg-transparent transition-all', {
+        'backdrop-blur-sm': isNavbarScrolled,
+      })}
+    >
+      <div className='layout flex items-center h-16'>
+        <nav className='w-full'>
+          {/* Desktop View Navbar */}
+          <div className='hidden w-full md:block'>
+            <div className='flex justify-between items-center w-full h-full'>
+              <img
+                src='/images/tedxits.png'
+                alt='TedxITS logo'
+                className='h-12'
+              />
+              <ul className='flex gap-16 items-center'>
+                {pageLinks.map(({ href, label }) => (
+                  <li key={`${href}${label}`}>
+                    <HeaderLink href={href} className='font-fivo text-lg'>
+                      {label}
+                    </HeaderLink>
+                  </li>
+                ))}
+              </ul>
+              <ul className='flex gap-2 items-center'>
+                {socialMediaLinks.map(({ href, label, logo }) => (
+                  <li key={`${href}${label}`}>{logo}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          {/* Mobile View Navbar */}
+          <div className='block relative items-center h-16 md:hidden'>
+            <div className='flex absolute inset-0 z-40 justify-between items-center w-full'>
+              <img
+                src='/images/tedxits.png'
+                alt='TedxITS logo'
+                className='h-12'
+              />
+              <ul className='flex justify-between items-center space-x-4'>
+                <button
+                  onClick={() => setIsNavOpen(!isNavOpen)}
+                  className='cursor-pointer'
+                >
+                  <Hamburger />
+                </button>
+              </ul>
+            </div>
+
+            {isNavOpen && (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{
+                  opacity: 1,
+                  transition: {
+                    duration: 0.05,
+                  },
+                }}
+                className='bg-cdark/90 fixed inset-0 z-30 h-screen transition-all'
+              >
+                <div className='layout min-h-main flex flex-col justify-center items-end pt-16'>
+                  <motion.ul
+                    className='flex flex-col gap-8'
+                    initial={{
+                      y: 60,
+                    }}
+                    animate={{
+                      y: 0,
+                      transition: {
+                        duration: 0.5,
+                        ease: [0.6, -0.05, 0.01, 0.99],
+                      },
+                    }}
+                  >
+                    {pageLinks.map(({ href, label }) => (
+                      <li key={`${href}${label}`}>
+                        <p className='text-right'>
+                          <HeaderLink href={href} className='text-2xl'>
+                            <p className='font-fivo text-2xl text-right'>
+                              {label}
+                            </p>
+                          </HeaderLink>
+                        </p>
+                      </li>
+                    ))}
+                  </motion.ul>
+                  <motion.ul
+                    className='flex gap-4 items-center mt-8'
+                    initial={{
+                      y: 60,
+                    }}
+                    animate={{
+                      y: 0,
+                      transition: {
+                        duration: 0.5,
+                        ease: [0.6, -0.05, 0.01, 0.99],
+                        delay: 0.5,
+                      },
+                    }}
+                  >
+                    {socialMediaLinks.map(({ href, label, logo }) => (
+                      <li key={`${href}${label}`}>{logo}</li>
+                    ))}
+                  </motion.ul>
+                </div>
+              </motion.div>
+            )}
+          </div>
         </nav>
       </div>
     </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -44,6 +44,20 @@ module.exports = {
         cpurple: '#780ae7',
         cpink: '#ffbcf0',
       },
+      fill: {
+        primary: {
+          50: withOpacity('--tw-clr-primary-50'),
+          100: withOpacity('--tw-clr-primary-100'),
+          200: withOpacity('--tw-clr-primary-200'),
+          300: withOpacity('--tw-clr-primary-300'),
+          400: withOpacity('--tw-clr-primary-400'),
+          500: withOpacity('--tw-clr-primary-500'),
+          600: withOpacity('--tw-clr-primary-600'),
+          700: withOpacity('--tw-clr-primary-700'),
+          800: withOpacity('--tw-clr-primary-800'),
+          900: withOpacity('--tw-clr-primary-900'),
+        },
+      },
       keyframes: {
         flicker: {
           '0%, 19.999%, 22%, 62.999%, 64%, 64.999%, 70%, 100%': {
