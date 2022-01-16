9d8cf07ab1b1e0183e2104c50ee3a896e10a37c8
winatungmiharja <winatungmiharja@gmail.com>
2022-01-16 22:07:11 +0900

    feat: optimizing sitemap for SEO

--- next-sitemap.js
+++ next-sitemap.js
@@ -3,7 +3,7 @@
  * @see https://github.com/iamvishnusankar/next-sitemap#readme
  */
 module.exports = {
-  siteUrl: 'https://www.tedxits.org',
+  siteUrl: 'https://www.tedxits.org/',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
