6a9a0abb16a4a95ee0f9d422fc43d036af4dd0c8
unknown <benewicaksono@gmail.com>
2022-02-16 00:41:27 +0700

    feat: merch page

---
+++ src/container/poMerch/DescriptionCard.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import clsxm from '@/lib/clsxm';
+
+import NextImage from '@/components/NextImage';
+
+type DescriptionCardProps = {
+  header: React.ReactNode;
+  description: React.ReactNode;
+  image: string;
+} & React.ComponentPropsWithoutRef<'div'>;
+
+export default function DescriptionCard({
+  header,
+  description,
+  image,
+  className,
+}: DescriptionCardProps) {
+  return (
+    <div className={clsxm('text-cdark text-cp-8 rounded-3xl', className)}>
+      {image && (
+        <NextImage
+          src={image}
+          width='100%'
+          height='100%'
+          alt='merch'
+          className='mb-7 w-full'
+          imgClassName='rounded-2xl'
+        />
+      )}
+      <h1 className='text-cred pb-2 text-lg'>{header}</h1>
+      <p className=''>{description}</p>
+    </div>
+  );
+}
