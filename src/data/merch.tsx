f89b96dacc259d3b7750db4ce4ba61321e813b83
unknown <benewicaksono@gmail.com>
2022-02-16 20:53:03 +0700

    fix: carousel + copywriting

--- src/data/merch.tsx
+++ src/data/merch.tsx
@@ -16,18 +16,18 @@ export const positionData: PositionData[] = [
     data: [
       {
         image: '/images/t-shirt.png',
-        title: <>T-SHIRT</>,
-        description: <>Rp150.000,00</>,
+        title: <>T-Shirt</>,
+        description: <>Rp90.000,00</>,
       },
       {
         image: '/images/totebag.png',
-        title: <>TOTEBAG</>,
-        description: <>Rp150.000,00</>,
+        title: <>Tote Bag</>,
+        description: <>Rp70.000,00</>,
       },
       {
         image: '/images/baseball-cap.png',
-        title: <>BASEBALL CAP</>,
-        description: <>Rp150.000,00</>,
+        title: <>Baseball Cap</>,
+        description: <>Rp55.000,00</>,
       },
     ],
     image: '',
@@ -37,18 +37,18 @@ export const positionData: PositionData[] = [
     data: [
       {
         image: '/images/keychain.png',
-        title: <>KEYCHAIN</>,
-        description: <>Rp150.000,00</>,
+        title: <>STNK Holder</>,
+        description: <>Rp30.000,00</>,
       },
       {
         image: '/images/pocket-hs.png',
-        title: <>POCKET HAND SANITIZER</>,
-        description: <>Rp150.000,00</>,
+        title: <>Pocket Handsanitizer</>,
+        description: <>Rp20.000,00</>,
       },
       {
         image: '/images/waterproof.png',
-        title: <>WATERPROOF WALLET</>,
-        description: <>Rp150.000,00</>,
+        title: <>Sako Wallet</>,
+        description: <>Rp35.000,00</>,
       },
     ],
     image: '',
@@ -57,9 +57,9 @@ export const positionData: PositionData[] = [
     dataClassName: 'grid-cols-1 gap-24 sm:grid-cols-3 md:gap-48',
     data: [
       {
-        image: '/images/keychain.png',
-        title: <>KEYCHAIN</>,
-        description: <>Rp150.000,00</>,
+        image: '/images/b-t-shirt.png',
+        title: <>Black Tshirt</>,
+        description: <>Rp90.000,00</>,
       },
     ],
     image: '',
