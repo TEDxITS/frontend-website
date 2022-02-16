833aae038e4760a41efc4091e5a6f7829538b20a
unknown <benewicaksono@gmail.com>
2022-02-16 20:53:03 +0700

    fix: carousel + copywriting

--- src/container/merch/CoverSection.tsx
+++ src/container/merch/CoverSection.tsx
@@ -7,7 +7,7 @@ import MerchPageCarousel from './MerchPageCarousel';
 
 export default function CoverSection() {
   return (
-    <div className='bg-cgray overflow-hidden relative z-40 md:min-h-main'>
+    <div className='bg-cgray overflow-hidden relative z-40 md:min-h-screen'>
       <NextImage
         src='/images/po-merch-bg.svg'
         width={800}
@@ -15,7 +15,7 @@ export default function CoverSection() {
         alt='bg'
         priority={true}
         quality={100}
-        className='-translate-x-[34rem] bottom-[50%] left-[50%] translate-y-[8rem] absolute scale-100 md:translate-y-[7rem] lg:-translate-x-[38rem] lg:scale-150'
+        className='-translate-x-[34rem] bottom-[50%] left-[50%] translate-y-[9.5rem] absolute scale-100 md:translate-y-[4rem] lg:-translate-x-[38rem] lg:scale-150'
       />
       <NextImage
         src='/images/po-merch-cover2.svg'
@@ -24,15 +24,7 @@ export default function CoverSection() {
         alt='bg'
         priority={true}
         quality={100}
-        className='-translate-x-[2rem] -translate-y-[6.5rem] bottom-[50%] left-[50%] scale-[.3] absolute z-50 sm:-translate-y-[10rem] md:-translate-x-[.5rem] md:-translate-y-[11.5rem] lg:-translate-y-[12.5rem]'
-      />
-      <NextImage
-        src='/images/po-merch-cover.svg'
-        width={32}
-        height={30}
-        alt='bg'
-        priority={true}
-        className='-translate-x-[10rem] bottom-[50%] left-[50%] translate-y-[9rem] absolute sm:translate-y-[10.5rem] md:-translate-x-[12.5rem] md:translate-y-[9.5rem] lg:translate-y-[8.5rem]'
+        className='-translate-x-[3rem] -translate-y-[7rem] bottom-[50%] left-[50%] scale-[.3] absolute z-50 sm:-translate-y-[12rem] md:-translate-x-[.5rem] md:-translate-y-[14.5rem] lg:-translate-y-[15.5rem]'
       />
       <div className='grid-cols layout min-h-[calc(100vh-64px-32px)] text-cdark grid relative z-10 lg:min-h-[calc(100vh-64px-64px)]'>
         <small className='font-fivo absolute left-0 top-8 text-sm md:top-16'>
@@ -46,17 +38,11 @@ export default function CoverSection() {
             <h1 className='text-cdark'>
               ARC<span className='font-pilow font-normal'>A</span>NE
             </h1>
-            <h1 className='text-cblue ml-11 text-3xl italic'>Secret</h1>
+            <h1 className='text-cblue ml-6 text-3xl italic sm:ml-11'>Secret</h1>
           </div>
           <div className='my-2 sm:my-4'>
-            <MerchPageCarousel className='w-[14rem] sm:w-[18rem]' />
+            <MerchPageCarousel className='w-[14rem] sm:w-[20rem]' />
           </div>
-          <h1 className='text-cdark'>
-            R<span className='font-pilow font-normal'>E</span>D BUNDLE
-          </h1>
-          <h4 className='mt-[-1rem] text-cdark max-w-lg sm:mt-2'>
-            Health Protocol is a Must!
-          </h4>
         </div>
         {/* Absolute item */}
         <small className='font-fivo absolute left-0 bottom-8 text-sm md:bottom-16'>
